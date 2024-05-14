084d4c249a657a06260c42c438d6c49961dfe906
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2024-05-14 23:30:53 +0900

    Update string.cpp

--- reference/string.cpp
+++ reference/string.cpp
@@ -16,27 +16,27 @@ int main() {
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
 
     // 여러가지 문자열의 초기화 방법
-    string str2("Hello");           
+    string str2("Hello");           // str2: H e l l o
     cout << str2 << endl;           // 출력: Hello
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | H| e| l| l| o|
 
-    string str3 = "World";          
+    string str3 = "World";          // str3: W o r l d
     cout << str3 << endl;           // 출력: World
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | W| o| r| l| d|
 
-    string str4(str2 + ", " + str3 + "!"); 
-    cout << str4 << endl;           // 출력: Hello, World!
+    string str4(str2 + ", " + str3 + "!"); // str4: H e l l o ,   W o r l d !
+    cout << str4 << endl;                  // 출력: Hello, World!
     // 인덱스:  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
 
-    string str5 = str1;             
+    string str5 = str1;             // str5: H e l l o ,   W o r l d !
     cout << str5 << endl;           // 출력: Hello, World!
     // 인덱스:  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
 
-    string str6(5, 'A');            
+    string str6(5, 'A');            // str6: A A A A A
     cout << str6 << endl;           // 출력: AAAAA
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | A| A| A| A| A|
@@ -65,6 +65,12 @@ int main() {
     // 인덱스:  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
 
+    /* 
+    + 연산자를 사용하여 문자열을 연결할 수 있습니다.
+    예: str9 = str7 + ", " + str8 + "!"; 
+    위의 코드는 str7과 ", ", str8, "!"를 연결하여 str9에 저장합니다.
+    */
+
     // 3. 문자열 길이와 접근
     cout << "Length of str1: " << str1.length() << endl;  // 출력: 13
     // str1의 길이: 13 (H e l l o ,   W o r l d !)
@@ -73,8 +79,11 @@ int main() {
     // str1의 첫 번째 문자: H (H e l l o ,   W o r l d !)
 
     /* 
-    주의사항:
-    - 문자열 인덱스를 사용하여 문자에 접근할 때 인덱스 범위를 초과하지 않도록 주의해야 합니다. 초과할 경우 undefined behavior가 발생합니다.
+    length() 메서드를 사용하여 문자열의 길이를 알 수 있습니다.
+    예: str1.length()는 13을 반환합니다.
+    
+    대괄호([])를 사용하여 문자열의 특정 위치에 있는 문자에 접근할 수 있습니다.
+    예: str1[0]은 'H'를 반환합니다.
     */
 
     // 4. 문자열의 부분 문자열 및 찾기
@@ -82,6 +91,12 @@ int main() {
     cout << "Substring: " << str10 << endl;  // 출력: World
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | W| o| r| l| d|
+
+    /* 
+    substr() 메서드를 사용하여 문자열의 부분 문자열을 추출할 수 있습니다.
+    예: str10 = str1.substr(7, 5);
+    위의 코드는 str1의 7번째 인덱스부터 시작하여 5개의 문자를 추출하여 str10에 저장합니다.
+    */
 
     size_t pos = str1.find("World");  
     if (pos != string::npos) {
@@ -91,6 +106,13 @@ int main() {
     }
     // str1:   | H| e| l| l| o| ,|  | W| o| r| l| d| !|
     // 인덱스: | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
+
+    /* 
+    find() 메서드를 사용하여 문자열 내에서 특정 문자열을 찾을 수 있습니다.
+    예: pos = str1.find("World");
+    위의 코드는 "World" 문자열이 str1에서 시작하는 인덱스를 반환하여 pos에 저장합니다.
+    만약 찾지 못하면 string::npos를 반환합니다.
+    */
 
     // 5. 문자열 대체
     string str11 = "I like cats";     
@@ -103,6 +125,13 @@ int main() {
     // str11:  | I|  | l| i| k| e|  | d| o| g| s|
     // 인덱스: | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|
 
+    /* 
+    replace() 메서드를 사용하여 문자열의 일부를 다른 문자열로 대체할 수 있습니다.
+    예: str11.replace(7, 4, "dogs");
+    위의 코드는 str11의 7번째 인덱스부터 4개의 문자를 "dogs"로 대체합니다.
+    */
+
     return 0;
 }
+
 
