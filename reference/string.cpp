f9fe9b2b1ce26b6bb532396a48b08c7403444b2a
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2024-05-15 13:36:06 +0900

    Update string.cpp

--- reference/string.cpp
+++ reference/string.cpp
@@ -14,32 +14,40 @@ int main() {
     cout << str1 << endl;           // 출력: Hello, World!
     // 인덱스:  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
+    // 시간 복잡도: O(n) (n은 문자열의 길이)
+    // 주의할 점: 문자열의 길이가 길어질수록 초기화에 시간이 더 걸립니다.
 
     // 여러가지 문자열의 초기화 방법
     string str2("Hello");           // str2: H e l l o
     cout << str2 << endl;           // 출력: Hello
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | H| e| l| l| o|
+    // 시간 복잡도: O(n)
 
     string str3 = "World";          // str3: W o r l d
     cout << str3 << endl;           // 출력: World
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | W| o| r| l| d|
+    // 시간 복잡도: O(n)
 
     string str4(str2 + ", " + str3 + "!"); // str4: H e l l o ,   W o r l d !
     cout << str4 << endl;                  // 출력: Hello, World!
     // 인덱스:  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
+    // 시간 복잡도: O(n + m) (n과 m은 각각 str2와 str3의 길이)
+    // 주의할 점: 문자열을 자주 연결하면 시간 복잡도가 커질 수 있습니다.
 
     string str5 = str1;             // str5: H e l l o ,   W o r l d !
     cout << str5 << endl;           // 출력: Hello, World!
     // 인덱스:  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
+    // 시간 복잡도: O(n)
 
     string str6(5, 'A');            // str6: A A A A A
     cout << str6 << endl;           // 출력: AAAAA
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | A| A| A| A| A|
+    // 시간 복잡도: O(n)
 
     /* 
     문자열(string)이란?
@@ -54,29 +62,29 @@ int main() {
     cout << str7 << endl;           // 출력: Hello
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | H| e| l| l| o|
+    // 시간 복잡도: O(n)
 
     string str8 = "World";           
     cout << str8 << endl;           // 출력: World
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | W| o| r| l| d|
+    // 시간 복잡도: O(n)
 
     string str9 = str7 + ", " + str8 + "!";  
     cout << str9 << endl;           // 출력: Hello, World!
     // 인덱스:  | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
     // 값    :  | H| e| l| l| o| ,|  | W| o| r| l| d| !|
-
-    /* 
-    + 연산자를 사용하여 문자열을 연결할 수 있습니다.
-    예: str9 = str7 + ", " + str8 + "!"; 
-    위의 코드는 str7과 ", ", str8, "!"를 연결하여 str9에 저장합니다.
-    */
+    // 시간 복잡도: O(n + m)
+    // 주의할 점: 여러 문자열을 연결할 때는 효율성을 고려해야 합니다.
 
     // 3. 문자열 길이와 접근
     cout << "Length of str1: " << str1.length() << endl;  // 출력: 13
     // str1의 길이: 13 (H e l l o ,   W o r l d !)
+    // 시간 복잡도: O(1)
 
     cout << "First character of str1: " << str1[0] << endl;  // 출력: H
     // str1의 첫 번째 문자: H (H e l l o ,   W o r l d !)
+    // 시간 복잡도: O(1)
 
     /* 
     length() 메서드를 사용하여 문자열의 길이를 알 수 있습니다.
@@ -91,12 +99,8 @@ int main() {
     cout << "Substring: " << str10 << endl;  // 출력: World
     // 인덱스:  | 0| 1| 2| 3| 4|
     // 값    :  | W| o| r| l| d|
-
-    /* 
-    substr() 메서드를 사용하여 문자열의 부분 문자열을 추출할 수 있습니다.
-    예: str10 = str1.substr(7, 5);
-    위의 코드는 str1의 7번째 인덱스부터 시작하여 5개의 문자를 추출하여 str10에 저장합니다.
-    */
+    // 시간 복잡도: O(m) (m은 부분 문자열의 길이)
+    // 주의할 점: substr의 인자는 시작 위치와 길이입니다.
 
     size_t pos = str1.find("World");  
     if (pos != string::npos) {
@@ -106,13 +110,8 @@ int main() {
     }
     // str1:   | H| e| l| l| o| ,|  | W| o| r| l| d| !|
     // 인덱스: | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|
-
-    /* 
-    find() 메서드를 사용하여 문자열 내에서 특정 문자열을 찾을 수 있습니다.
-    예: pos = str1.find("World");
-    위의 코드는 "World" 문자열이 str1에서 시작하는 인덱스를 반환하여 pos에 저장합니다.
-    만약 찾지 못하면 string::npos를 반환합니다.
-    */
+    // 시간 복잡도: O(n)
+    // 주의할 점: find 메서드는 찾는 문자열의 시작 위치를 반환하며, 없으면 string::npos를 반환합니다.
 
     // 5. 문자열 대체
     string str11 = "I like cats";     
@@ -124,14 +123,11 @@ int main() {
     cout << str11 << endl;  // 출력: I like dogs
     // str11:  | I|  | l| i| k| e|  | d| o| g| s|
     // 인덱스: | 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|
-
-    /* 
-    replace() 메서드를 사용하여 문자열의 일부를 다른 문자열로 대체할 수 있습니다.
-    예: str11.replace(7, 4, "dogs");
-    위의 코드는 str11의 7번째 인덱스부터 4개의 문자를 "dogs"로 대체합니다.
-    */
+    // 시간 복잡도: O(m) (m은 대체할 문자열의 길이)
+    // 주의할 점: replace의 인자는 시작 위치, 길이, 대체할 문자열입니다.
 
     return 0;
 }
+
 
 
