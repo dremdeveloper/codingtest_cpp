9051368a3b74c1f60113989ff1c8d9a599eae572
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2024-05-22 00:44:09 +0900

    Update next_permutation.cpp

--- STL/next_permutation.cpp
+++ STL/next_permutation.cpp
@@ -5,37 +5,77 @@
 //############################################################
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
+#include <algorithm> // for std::next_permutation and std::sort
 using namespace std;
 
-// 순열은 가능한 모든 원소의 조합을 의미합니다.
-// - STL의 next_permutation은 주어진 범위의 원소를 재배치하여 사전 순으로 다음 순열을 생성합니다.
-// 좋은 사용 시기:
-// - 가능한 모든 원소의 조합을 순차적으로 생성하고 싶을 때. 예를 들면, 완전 탐색 알고리즘에서 사용될 수 있습니다.
-// 성능 이슈:
-// - next_permutation은 원소의 수에 따라 많은 수의 순열을 생성할 수 있습니다. 원소의 수가 많아지면 가능한 모든 순열을 생성하는 데 많은 시간이 걸릴 수 있습니다.
-
-int main() {
-
-    vector<int> v = {1, 2, 3};
-
-    // next_permutation: 주어진 범위의 원소를 재배치하여 다음 순열을 생성, O(n)
+// 벡터의 모든 순열을 출력하는 함수
+void print_permutations(vector<int> v) {
     do {
-        for(int i : v) {
-            cout << i << " ";
+        // 현재 순열 출력
+        for (int num : v) {
+            cout << num << " ";
         }
         cout << endl;
     } while (next_permutation(v.begin(), v.end()));
+}
 
-    // 출력:
-    // 1 2 3 
-    // 1 3 2 
-    // 2 1 3 
-    // 2 3 1 
-    // 3 1 2 
-    // 3 2 1 
+int main() {
+    // 예시 벡터 생성
+    vector<int> v = {3, 2, 1};
+
+    // 정렬되지 않은 경우
+    cout << "정렬되지 않은 경우:\n";
+    vector<int> v_unsorted = v;
+    print_permutations(v_unsorted);
+
+    // 벡터를 정렬된 상태로 만들기
+    sort(v.begin(), v.end());
+
+    // 정렬된 경우
+    cout << "\n정렬된 경우:\n";
+    vector<int> v_sorted = v;
+    print_permutations(v_sorted);
 
     return 0;
 }
+
+/*
+출력값:
+
+정렬되지 않은 경우:
+3 2 1
+
+정렬된 경우:
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+*/
+
+// next_permutation에 대한 설명
+/*
+std::next_permutation 함수는 주어진 범위에서 사전식 순서로 다음 순열을 생성합니다.
+이 함수는 내부적으로 요소의 순서를 비교하여 다음 순열을 만듭니다.
+
+주의해야 할 점:
+1. 정렬된 상태에서 사용: next_permutation 함수는 범위가 정렬된 상태에서 시작할 때 모든 순열을
+   올바르게 생성할 수 있습니다. 정렬되지 않은 상태에서 시작하면 모든 순열을 생성하지 못합니다.
+2. 반환값: 다음 순열이 존재하면 true를 반환하고, 더 이상 다음 순열이 없으면 false를 반환하여
+   주어진 범위를 처음 순열(가장 작은 순열)로 재설정합니다.
+3. 시간 복잡도: 함수 자체의 시간 복잡도는 O(N)입니다. 모든 순열을 생성하기 위해 반복적으로
+   호출하면 총 시간 복잡도는 O(N * N!)이 됩니다. 여기서 N은 요소의 수입니다.
+*/
+
+// next_permutation 사용 예:
+/*
+벡터가 {1, 2, 3}로 주어졌을 때,
+next_permutation(v.begin(), v.end())를 호출하면,
+벡터는 {1, 3, 2}로 변환됩니다.
+
+벡터가 {3, 2, 1}로 주어졌을 때,
+next_permutation(v.begin(), v.end())를 호출하면,
+벡터는 다시 {1, 2, 3}로 재설정됩니다.
+*/
 
