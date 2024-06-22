11876749596cb8ff34e3bde10fa1ba7fcc3d4b31
Kyungrok Park <131899974+dremdeveloper@users.noreply.github.com>
2024-06-23 00:06:22 +0900

    Update adjacencyarray.cpp

--- Algorithm_with_DataStructure/adjacencyarray.cpp
+++ Algorithm_with_DataStructure/adjacencyarray.cpp
@@ -4,63 +4,77 @@
 // | business   | ultrasuperrok@gmail.com                    |
 //############################################################
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-/*
-그래프의 최종 모습 (인접 행렬 형식):
-Adjacency Matrix:
-0 1 0 0 1
-1 0 1 1 1
-0 1 0 1 0
-0 1 1 0 1
-1 1 0 1 0
-이 구조는 무방향 그래프를 나타내며, 각 정점은 해당하는 다른 정점들과 1로 표시된 연결을 가집니다.
-시간 복잡도 설명:
-- addEdge 함수는 O(1)
-- printGraph 함수는 O(V^2)
-*/
+// 인접 행렬의 노드 개수 (고정)
+const int numNodes = 2;
 
+// 인접 행렬 배열
+int adjMatrix[numNodes][numNodes];
 
-// addEdge 함수는 그래프의 인접 행렬에 양방향 간선을 추가합니다.
-// 'u'와 'v'는 연결될 두 정점입니다. 'adjMatrix'는 인접 행렬을 나타내는 2차원 벡터입니다.
-// 이 함수의 시간 복잡도는 O(1), 특정 행렬 위치에 접근하여 값을 설정하는데 상수 시간이 걸립니다.
-void addEdge(vector<vector<int>>& adjMatrix, int u, int v) {
-    adjMatrix[u][v] = 1;  // 정점 u에서 v로의 간선을 추가
-    adjMatrix[v][u] = 1;  // 무방향 그래프이므로, 정점 v에서 u로도 간선을 추가
+// 인접 행렬을 초기화하는 함수
+// 모든 값을 0으로 설정
+void initializeMatrix() {
+    for (int i = 0; i < numNodes; i++) {
+        for (int j = 0; j < numNodes; j++) {
+            adjMatrix[i][j] = 0;
+        }
+    }
 }
 
-// printGraph 함수는 그래프의 인접 행렬을 출력합니다.
-// 각 행과 열을 순회하며, 각 정점간 연결 상태를 출력합니다.
-// 시간 복잡도는 O(V^2) 입니다. 여기서 V는 정점의 수입니다.
-void printGraph(const vector<vector<int>>& adjMatrix) {
-    cout << "Adjacency Matrix:\n";
-    for (int i = 0; i < adjMatrix.size(); ++i) {
-        for (int j = 0; j < adjMatrix[i].size(); ++j) {
-            cout << adjMatrix[i][j] << " ";
+// 간선을 추가하는 함수
+// u에서 v로 가는 간선의 가중치를 설정
+void addEdge(int u, int v, int weight) {
+    adjMatrix[u][v] = weight;
+}
+
+// 인접 행렬을 출력하는 함수
+// 값이 0인 경우 '-'로 출력
+void printMatrix() {
+    for (int i = 0; i < numNodes; i++) {
+        for (int j = 0; j < numNodes; j++) {
+            if (adjMatrix[i][j] == 0)
+                cout << "- ";
+            else
+                cout << adjMatrix[i][j] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
 }
 
 int main() {
-    int V = 5; // 그래프의 정점 개수
-    vector<vector<int>> adjMatrix(V, vector<int>(V, 0)); // 모든 값을 0으로 초기화된 VxV 크기의 2차원 벡터 생성
+    // 인접 행렬 초기화
+    initializeMatrix();
 
-    // 간선 추가
-    addEdge(adjMatrix, 0, 1);
-    addEdge(adjMatrix, 0, 4);
-    addEdge(adjMatrix, 1, 2);
-    addEdge(adjMatrix, 1, 3);
-    addEdge(adjMatrix, 1, 4);
-    addEdge(adjMatrix, 2, 3);
-    addEdge(adjMatrix, 3, 4);
+    // 엣지 추가: 서울(0)에서 부산(1)으로 가는 엣지, 가중치 400
+    addEdge(0, 1, 400);
 
-    // 그래프 출력
-    printGraph(adjMatrix);
+    // 인접 행렬 출력
+    printMatrix();
+
+    /*
+    최종적인 인접 행렬의 모습:
+    - 400 
+    -  - 
+    */
 
     return 0;
 }
+
+/*
+인접 행렬 설명:
+- 인접 행렬은 그래프를 행렬 형태로 표현하는 방법입니다.
+- 행과 열의 인덱스는 각각의 노드를 나타내고, 행렬의 값은 간선의 가중치를 나타냅니다.
+- 만약 간선이 없는 경우 0으로 표시되며, 이 코드에서는 '-'로 출력됩니다.
+
+인접 행렬의 시간 복잡도:
+- 인접 행렬을 사용하여 그래프를 표현할 경우, 공간 복잡도는 O(V^2)입니다. 여기서 V는 노드의 개수입니다.
+- 간선이 존재하는지 확인하는 작업은 O(1)의 시간 복잡도를 가집니다.
+- 모든 간선을 탐색하는 작업은 O(V^2)의 시간 복잡도를 가집니다.
+
+최종적인 인접 행렬의 모습:
+- 400 
+-  - 
+*/
 
 
